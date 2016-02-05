d2abb6f24fd200ff6ef18d298eaefc356d94928d
Kyle <kcollie@hotmail.com>
2016-02-04 23:55:21 -0500

    Finished Identity

---
+++ src/TheWorld/Controllers/AuthController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheWorld.Models;
+using TheWorld.ViewModels;
+
+namespace TheWorld.Controllers
+{
+    public class AuthController : Controller
+    {
+        private SignInManager<WorldUser> _singInManager;
+
+        public AuthController(SignInManager<WorldUser> singInManager)
+        {
+            _singInManager = singInManager;
+        }
+
+        public IActionResult Login()
+        {
+
+            if (User.Identity.IsAuthenticated)
+            {
+                return RedirectToAction("Trips", "App");
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginViewModel vm, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var signInResult = await _singInManager.PasswordSignInAsync(vm.Username,
+                                                                      vm.Password,
+                                                                      true, false);
+
+                if (signInResult.Succeeded)
+                {
+                    if (string.IsNullOrWhiteSpace(returnUrl))
+                    {
+                        return RedirectToAction("Trips", "App");
+                    }
+                    else
+                    {
+                        return Redirect(returnUrl);
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Username or password incorrect");
+                }
+            }
+
+            return View();
+        }
+
+        public async Task<ActionResult> Logout()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                await _singInManager.SignOutAsync();
+            }
+
+            return RedirectToAction("Index", "App");
+        }
+    }
+}
--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -1,11 +1,16 @@
 ﻿using AutoMapper;
+using Microsoft.AspNet.Authentication.Cookies;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.PlatformAbstractions;
 using Newtonsoft.Json.Serialization;
+using System.Net;
+using System.Threading.Tasks;
 using TheWorld.Models;
 using TheWorld.Serices;
 using TheWorld.Services;
@@ -31,11 +36,41 @@ namespace TheWorld
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc()
-                .AddJsonOptions(opt =>
+            services.AddMvc(config => 
+            {
+#if !DEBUG
+                config.Filters.Add(new RequireHttpsAttribute());
+#endif
+            })
+            .AddJsonOptions(opt =>
+            {
+                opt.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            });
+
+            services.AddIdentity<WorldUser, IdentityRole>(config =>
+            {
+                config.User.RequireUniqueEmail = true;
+                config.Password.RequiredLength = 8;
+                config.Cookies.ApplicationCookie.LoginPath = "/Auth/Login";
+                config.Cookies.ApplicationCookie.Events = new CookieAuthenticationEvents()
                 {
-                    opt.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-                });
+                    OnRedirectToLogin = ctx =>
+                    {
+                        if (ctx.Request.Path.StartsWithSegments("/api") &&
+                            ctx.Response.StatusCode == (int)HttpStatusCode.OK)
+                        {
+                            ctx.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                        }
+                        else
+                        {
+                            ctx.Response.Redirect(ctx.RedirectUri);
+                        }
+
+                        return Task.FromResult(0);
+                    }
+                };
+            })
+            .AddEntityFrameworkStores<WorldContext>();
 
             services.AddLogging();
 
@@ -55,11 +90,13 @@ namespace TheWorld
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, WorldContextSeedData seeder, ILoggerFactory loggerFactory)
+        public async void Configure(IApplicationBuilder app, WorldContextSeedData seeder, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddDebug(LogLevel.Warning);
 
             app.UseStaticFiles();
+
+            app.UseIdentity();
 
             Mapper.Initialize(config =>
             {
@@ -77,7 +114,7 @@ namespace TheWorld
                     );
             });
 
-            seeder.EnsureSeedData();
+            await seeder.EnsureSeedDataAsync();
         }
 
         // Entry point for the application.
